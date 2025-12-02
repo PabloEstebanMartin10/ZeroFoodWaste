073c4abf39ada54403931525eaab3d6705799d4b
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 16:48:12 +0100

    Add: DonationAssignment

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/DonationAssignment.java
@@ -0,0 +1,37 @@
+package com.example.ZeroFoodWaste.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class DonationAssignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "donationId", nullable = false)
+    private Donation donation;
+
+    @ManyToOne
+    @JoinColumn(name = "foodBankId", nullable = false)
+    private FoodBank foodBank;
+
+    @Column(nullable = false)
+    private LocalDateTime acceptedAt;
+
+    private LocalDateTime pickedUpAt;
+
+    @PrePersist
+    public void prePersist(){
+        acceptedAt = LocalDateTime.now();
+    }
+}
