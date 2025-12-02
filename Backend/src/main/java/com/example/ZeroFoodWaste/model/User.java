21d01025988f520cd478f71cbd7cfcd2cb431b64
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 15:39:50 +0100

    Add: Enum Role & Entidad user

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/User.java
@@ -0,0 +1,28 @@
+package com.example.ZeroFoodWaste.model;
+
+import com.example.ZeroFoodWaste.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
