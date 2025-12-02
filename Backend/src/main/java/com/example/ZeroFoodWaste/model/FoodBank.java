70e0bc98bd4aed595ff953d11e2fc51fab2d670f
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 16:49:03 +0100

    relacion DonationAssingment entity & FoodBank entity

--- Backend/src/main/java/com/example/ZeroFoodWaste/model/FoodBank.java
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/FoodBank.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -30,4 +32,6 @@ public class FoodBank {
     @Column(nullable = false)
     private String coverageArea;
 
+    @OneToMany(mappedBy = "foodBank")
+    private List<DonationAssignment> assignments;
 }
