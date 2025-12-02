2ef8b9a33e0b021477d3ce30ad695e51d38087bc
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 18:06:54 +0100

    feat: create user & login

--- Backend/src/main/java/com/example/ZeroFoodWaste/service/UserService.java
+++ Backend/src/main/java/com/example/ZeroFoodWaste/service/UserService.java
@@ -1,13 +1,28 @@
 package com.example.ZeroFoodWaste.service;
 
+import com.example.ZeroFoodWaste.model.User;
 import com.example.ZeroFoodWaste.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    public User LoginUser(String email, String passwordHash) {
+        return userRepository.findByEmailPasswordHash(email, passwordHash).orElseThrow(
+                () -> new NoSuchElementException("Invalid user or password "));
+    }
+
 
 
 }
