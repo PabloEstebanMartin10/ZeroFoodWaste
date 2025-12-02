9c5610acde68dd95d71a2ffbdd696aca025a0bbd
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 18:00:03 +0100

    feat: findby email & password

--- Backend/src/main/java/com/example/ZeroFoodWaste/repository/UserRepository.java
+++ Backend/src/main/java/com/example/ZeroFoodWaste/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.ZeroFoodWaste.repository;
 import com.example.ZeroFoodWaste.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailPasswordHash(String email, String passwordHash);
 }
