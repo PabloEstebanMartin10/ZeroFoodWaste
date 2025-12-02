0494d848fc4efeec02bde3473c05931ab7fe53d5
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 18:45:44 +0100

    add: FoodBankRepository + feat: findByUserId

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/repository/FoodBankRepository.java
@@ -0,0 +1,10 @@
+package com.example.ZeroFoodWaste.repository;
+
+import com.example.ZeroFoodWaste.model.FoodBank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FoodBankRepository extends JpaRepository<FoodBank, Long> {
+    Optional<FoodBank> findByUserId(Long userId);
+}
