0a1c55b418bbc6f4f3a26df63a34f5c7083ba609
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-02 15:55:26 +0100

    Add: Establishment entity

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/Establishment.java
@@ -0,0 +1,34 @@
+package com.example.ZeroFoodWaste.model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Establishment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "userId", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String contactPhone;
+
+    @Column(nullable = false)
+    private String openingHours;
+}
