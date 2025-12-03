3f38ec94ae5e94a06f6d5ebcd1eb6ab4ed865a27
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-03 19:54:37 +0100

    add: Javadoc to DonationService

--- Backend/src/main/java/com/example/ZeroFoodWaste/service/DonationService.java
+++ Backend/src/main/java/com/example/ZeroFoodWaste/service/DonationService.java
@@ -24,6 +24,13 @@ import java.util.Objects;
 @Service
 @RequiredArgsConstructor
 public class DonationService {
+    /* todos
+    todo 1 dtos en general y mapeo pero en especial los que tienen su comentario propio
+    todo 2 metodo de busqueda con throw para no tener que repetir y que reciba la excepción que debe lanzar
+    todo 3 hacer las excepciones mas especificas a mano como por ejemplo: DonationNotFoundException
+    todo 4 evitar devolver entidades
+    todo 5 añadir @transactional a los metodos
+     */
 
     //region repositories
     private final DonationRepository donationRepository;
@@ -33,12 +40,27 @@ public class DonationService {
     //endregion
 
     //region get
+
+    /**
+     * Obtains the list of donations with a specific status
+     *
+     * @param statusStr determines the status we search for
+     *                  transforms into {@link DonationStatus}
+     * @return List<Donation> returns all donations with the status specified
+     */
     public List<Donation> getDonations(String statusStr) {
         DonationStatus status = DonationStatus.valueOf(statusStr.trim().toUpperCase());
         return donationRepository.findByStatus(status);
     }
 
-    public Donation getDonation(Long id) {
+    /**
+     * Obtains a specific donation by donation id
+     *
+     * @param id donation id to search for a specific donation
+     * @return the donation if found
+     * @throws NoSuchElementException if it can't find the donation
+     */
+    public Donation getDonation(Long id) throws NoSuchElementException {
         return donationRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Couldn't find the Donation")
         );
@@ -46,7 +68,19 @@ public class DonationService {
     //endregion
 
     //region post
-    //todo pasar a dto y mapper
+
+    /**
+     * todo pasar a dto y mapper
+     *  Creates a donation and save in the DB
+     *
+     * @param establishmentId transforms into {@link Establishment} establishment that created the donation
+     * @param productName     String with the name of the product
+     * @param description     String with a description of the product(optional)
+     * @param quantity        String that specifies quantity and uds,kg, etc.
+     * @param expirationDate  LocalDateTime that specifies the expiration date
+     * @param status          transforms into {@link DonationStatus} to select the status of the donation
+     * @return Donation that has been created
+     */
     public Donation createDonation(Long establishmentId, String productName, String description, String quantity, LocalDateTime expirationDate, String status) {
         Establishment establishment = establishmentRepository.findById(establishmentId).orElseThrow(
                 () -> new NoSuchElementException("Couldn't find the Establishment")
@@ -58,6 +92,13 @@ public class DonationService {
     //endregion
 
     //region delete
+
+    /**
+     * Search a donation and delete it
+     *
+     * @param id id of the donation to delete
+     * @return the donation deleted
+     */
     public Donation deleteDonation(Long id) {
         Donation donation = donationRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Couldn't find the Donation")
@@ -68,20 +109,21 @@ public class DonationService {
     //endregion
 
     //region put/patch
-    public Donation acceptDonation(Long id, Long foodBankId) {
-        Donation donation = donationRepository.findById(id).orElseThrow(
-                () -> new NoSuchElementException("Couldn't find the Donation")
-        );
-        FoodBank foodBank = foodBankRepository.findById(foodBankId).orElseThrow(
-                () -> new NoSuchElementException("Couldn't find the Food Bank")
-        );
-        DonationAssignment assignment = new DonationAssignment(donation, foodBank);
-        assignmentRepository.save(assignment);
-        donation.setStatus(DonationStatus.ACCEPTED);
-        return donationRepository.save(donation);
-    }
 
-    //todo pasar a dto
+    /**
+     * todo pasar a dto
+     * modiifies the values of the donations
+     *
+     * @param id              (not modified) the donation that is modified
+     * @param establishmentId (not modified) transform into {@link Establishment} to check if the establishment
+     *                        modifying is the same that created the donation
+     * @param productName     String with the name of the product being donated
+     * @param description     (optional) String with the description of the product being donated
+     * @param quantity        String with the quantity and uds,kg, etc.
+     * @param expirationDate  LocalDateTime with the expiration date
+     * @param status          transforms into {@link DonationStatus} specifies the status of the donation
+     * @return the donation modified
+     */
     public Donation modifyDonation(Long id, Long establishmentId, String productName, String description, String quantity, LocalDateTime expirationDate, String status) {
         Donation modifyDonation = donationRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Couldn't find the Donation")
@@ -99,6 +141,31 @@ public class DonationService {
         }
     }
 
+    /**
+     *
+     * @param id         id of the donation accepted
+     * @param foodBankId transform into {@link FoodBank} the food bank that accepted the donation
+     * @return the donation with the status changed
+     */
+    public Donation acceptDonation(Long id, Long foodBankId) {
+        Donation donation = donationRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Couldn't find the Donation")
+        );
+        FoodBank foodBank = foodBankRepository.findById(foodBankId).orElseThrow(
+                () -> new NoSuchElementException("Couldn't find the Food Bank")
+        );
+        DonationAssignment assignment = new DonationAssignment(donation, foodBank);
+        assignmentRepository.save(assignment);
+        donation.setStatus(DonationStatus.ACCEPTED);
+        return donationRepository.save(donation);
+    }
+
+    /**
+     * modifies the status of the donation selected to picked up
+     *
+     * @param id id of the donation to be picked up
+     * @return the donation modified
+     */
     public Donation pickUpDonation(Long id) {
         Donation donation = donationRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Couldn't find the Donation")
@@ -111,5 +178,6 @@ public class DonationService {
         assignmentRepository.save(assignment);
         return donationRepository.save(donation);
     }
+
     //endregion
 }
