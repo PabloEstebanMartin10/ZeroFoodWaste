4aead4763e9dacb5864eac2b245b757433ca88f6
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-03 19:55:03 +0100

    add: regions & javadoc to EstablishmentService

--- Backend/src/main/java/com/example/ZeroFoodWaste/service/EstablishmentService.java
+++ Backend/src/main/java/com/example/ZeroFoodWaste/service/EstablishmentService.java
@@ -1,3 +1,5 @@
+//region imports
+
 package com.example.ZeroFoodWaste.service;
 
 import com.example.ZeroFoodWaste.model.Establishment;
@@ -8,17 +10,45 @@ import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
+//endregion
+
 @Service
 @RequiredArgsConstructor
 public class EstablishmentService {
+    /* todos
+    todo 1 añadir @transactional
+    todo 2 excepciones personalizadas
+    todo 3 dtos para no usar entities
+    todo 4 cambiar el mapper de beanUtils
+     */
     private final EstablishmentRepository establishmentRepository;
 
+    //region get
 
+    /**
+     * Retrieves an establishment associated with a specific user ID.
+     *
+     * @param userId the ID of the user whose establishment is to be retrieved
+     * @return the {@link Establishment} linked to the user
+     * @throws NoSuchElementException if no establishment is found for the given user ID
+     */
     public Establishment getEstablishment(Long userId){
         return establishmentRepository.findByUserId(userId).orElseThrow(
                 ()->new NoSuchElementException("Couldn't find the establishment"));
     }
 
+    //endregion
+
+    //region put/patch
+
+    /**
+     * Updates an existing establishment with new values.
+     * Only the fields allowed for modification will be updated; ID and user association are not changed.
+     *
+     * @param est an {@link Establishment} object containing the new values
+     * @return the updated {@link Establishment} after saving the changes
+     * @throws NoSuchElementException if the establishment with the given ID does not exist
+     */
     public Establishment modifyEstablishment(Establishment est){
         Establishment establishment = establishmentRepository.findById(est.getId()).orElseThrow(
                 ()->new NoSuchElementException("Couldn't find the establishment")
@@ -26,4 +56,6 @@ public class EstablishmentService {
         BeanUtils.copyProperties(est,establishment,"id","user","donations");
         return establishmentRepository.save(establishment);
     }
+
+    //endregion
 }
