d18045999437b130e77a5f0337baccc3a0106df4
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-09 19:28:41 +0100

    add: NewUserDTO

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/dto/NewUserDTO.java
@@ -0,0 +1,29 @@
+package com.example.ZeroFoodWaste.model.dto;
+
+import com.example.ZeroFoodWaste.model.enums.Role;
+import lombok.Data;
+
+@Data
+public class NewUserDTO {
+    //region userData
+    private String email;
+    private String password;
+
+    private Role role;
+    //endregion
+
+    //region EstablishmentData
+    private String establishmentName;
+    private String establishmentAddress;
+    private String establishmentContactPhone;
+    private String openingHours;
+    //endregion
+
+    //region FoodBankData
+    private String foodBankName;
+    private String foodBankAddress;
+    private String foodBankContactPhone;
+    private String coverageArea;
+    //endregion
+
+}
