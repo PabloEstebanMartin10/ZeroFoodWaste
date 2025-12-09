098bf0dc535480c5cf17603fe982e58f20272565
Pablo Esteban <estebanmartinpablo@gmail.com>
2025-12-09 19:30:04 +0100

    add: UserResponseMapper

---
+++ Backend/src/main/java/com/example/ZeroFoodWaste/model/mapper/UserResponseMapper.java
@@ -0,0 +1,14 @@
+package com.example.ZeroFoodWaste.model.mapper;
+
+import com.example.ZeroFoodWaste.model.dto.UserResponseDTO;
+import com.example.ZeroFoodWaste.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public abstract class UserResponseMapper {
+
+    @Mapping(target = "establishmentId", expression = "java(user.getEstablishment() != null ? user.getEstablishment().getId() : null)")
+    @Mapping(target = "foodBankId", expression = "java(user.getFoodBank() != null ? user.getFoodBank().getId() : null)")
+    public abstract UserResponseDTO toDTO(User user);
+}
